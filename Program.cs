2471fa1801b9a1a0a804d88e374fd4bc776f9760
Amahle Nyangani <amahle.nyangani@dutappfactory.co.za>
2024-04-03 07:11:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace FizzBuzz
+{
+	public class Program
+	{
+		[STAThread]
+		public static void Main()
+		{
+			FizzBuzz();
+		}
+
+		public static void FizzBuzz()
+		{
+			for (int num = 0; num < 14; num++)
+			{
+				if (num % 3 == 0)
+				{
+					Console.WriteLine("Fizz");
+				}
+				else if
+					(num % 5 == 0)
+				{
+					Console.WriteLine("Buzz");
+				}
+				else
+				{
+					Console.WriteLine(num);
+				}
+			}
+
+		}
+	}
+}
+
